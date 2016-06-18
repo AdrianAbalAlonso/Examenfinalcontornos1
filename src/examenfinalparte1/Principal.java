6fad08cef2c85c06b1458146375c22dbe834b4f1
Adrián <Adrin@EquipoAdri>
2016-06-18 13:57:43 +0200

    rename variables clase principal

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -10,14 +10,23 @@ public class Principal {
 
     public static void main(String[] args) throws IOException {
         
-        Operacion obj = new Operacion(10, 20);
-        int n = obj.resultadoSum;
-        System.out.println("Es: " + n);
-        Operacion nm = new Operacion(10);
-        int m = nm.resultadoSum;
-        System.out.println("Es: " + m);
-        Operacion mn = new Operacion();
-        int nn = mn.resultadoSum;
-        System.out.println("Es: " + nn);
+        Operacion constructor1 = new Operacion(10, 20);
+        
+        int resultado1 = constructor1.resultadoSum;
+        
+        System.out.println("Es: " + resultado1);
+        
+        Operacion constructor2 = new Operacion(10);
+        
+        int resultado2 = constructor2.resultadoSum;
+        
+        System.out.println("Es: " + resultado2);
+        
+        Operacion constructor3 = new Operacion();
+        
+        int resultado3 = constructor3.resultadoSum;
+        
+        System.out.println("Es: " + resultado3);
+        
     }
 }
